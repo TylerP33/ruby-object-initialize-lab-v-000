b23e5dfe751d2d5d8754e27dcf2b924505210e4a
Tyler Pulse <tyler3321@gmail.com>
2017-10-16 22:06:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,21 +2,23 @@ class dog
   def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
-  end 
+  end
 
   def name=(name)
     @name = name
-  end 
+  end
 
   def breed=(breed)
     @breed = breed
-  end 
+  end
 
   def name
     @name
-  end 
+  end
 
   def breed
     @breed
-  end 
+  end
 end 
+
+
